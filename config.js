d78a70bb332d6670c911119a840493996f223930
mn15104 <minhnguyen1995@googlemail.com>
2017-09-23 17:25:18 +0100

    m

---
+++ config.js
@@ -0,0 +1,17 @@
+var config = function(){
+
+}
+
+config.username = function(){
+    return "minnigayuen";
+}
+
+config.password = function(){
+    return "PW071095C00k135?";
+}
+
+config.db = function(){
+    return "mydb";
+}
+
+module.exports = config;
