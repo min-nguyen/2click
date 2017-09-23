0ec15bf8b436013812614804492c787be5b22338
mn15104 <minhnguyen1995@googlemail.com>
2017-09-23 17:32:29 +0100

    m

---
+++ config.js
@@ -0,0 +1,17 @@
+var config = function(){
+
+}
+
+config.username = function(){
+    return "root";
+}
+
+config.password = function(){
+    return "M0in1~mi9N";
+}
+
+config.db = function(){
+    return "mydb";
+}
+
+module.exports = config;
